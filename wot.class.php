7f78634bb932921ac140d62d5bc91cf51ed9bdc2
Simon Wallmann <s.wallmann@kompsoft.de>
2015-01-08 20:20:31 +0100

    Bump version to 1.0.0

--- wot.class.php
+++ wot.class.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
 if(!class_exists('wot')) {
 	class wot extends game_generic {
 		protected static $apiLevel	= 20;
-		public $version				= '0.5';
+		public $version				= '1.0.0';
 		protected $this_game		= 'wot';
 		protected $types			= array('classes', 'races');
 		protected $classes			= array();
